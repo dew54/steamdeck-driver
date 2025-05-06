b74ce6c1cedbd406650c4fa758a8c1f65646dd8a
dew54 <dvdvdm96@gmail.com>
2025-05-06 18:54:39 +0200

    add

--- roboPlatform.py
+++ roboPlatform.py
@@ -4,7 +4,8 @@ from steamdeck import SteamDeck
 
 class Platform:
     def __init__(self, deck: SteamDeck, nKinetics=1):
-        kinetic_0 = Kinetic(0)
+        self.deck = deck
+        self.kinetic_0 = Kinetic(0)
 
         # for i in range(nKinetics):
         #     setattr(self, f'kinetic_{i}', Kinetic(i))
